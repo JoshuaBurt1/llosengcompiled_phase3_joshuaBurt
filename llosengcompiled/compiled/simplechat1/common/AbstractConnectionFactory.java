780a3b69af7ebb9dcf39856f5f8aa920914e8ad7
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2025-11-30 22:02:55 -0500

    Update AbstractConnectionFactory.java
    general comment

--- llosengcompiled/compiled/simplechat1/common/AbstractConnectionFactory.java
+++ llosengcompiled/compiled/simplechat1/common/AbstractConnectionFactory.java
@@ -4,6 +4,7 @@ import ocsf.server.ConnectionToClient;
 import java.io.IOException;
 import java.net.Socket;
 
+//this creates an interface for a server to connect to a client in AbstractServer, AdaptableServer, and ObservableServer
 public interface AbstractConnectionFactory {
     ConnectionToClient createConnection(
             ThreadGroup group,
